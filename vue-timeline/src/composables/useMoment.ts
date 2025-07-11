d8d95d323c3a4ba704d655320a7896818cc7a247
Ben Koplin <benjaminkoplin@outlook.com>
2025-07-11 05:10:56 -0500

    feat: enhance moment integration by refining type definitions and imports

--- vue-timeline/src/composables/useMoment.ts
+++ vue-timeline/src/composables/useMoment.ts
@@ -1,6 +1,9 @@
-import { default as momentMoment } from 'moment'
+import type { DateRange, MomentRange } from 'moment-range'
+import momentMoment from 'moment'
 import { extendMoment } from 'moment-range'
 
-export const moment = extendMoment(momentMoment)
+export type { unitOfTime } from 'moment'
+
+export const moment = extendMoment(momentMoment) as unknown as ReturnType<typeof momentMoment> & MomentRange
 
 export const useMomentMethods = reactifyObject(moment)
